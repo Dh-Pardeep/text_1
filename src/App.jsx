f43735f9feec14dba73a876abbcbedcaf98d3e77
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-12-05 00:09:22 +0530

    meta img

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,6 @@ function App() {
       duration: 1500,
     })
   }, [])
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-      if (targetElement) {
-        targetElement.scrollIntoView({
-          behavior: 'smooth'
-        });
-      }
-    });
-  });
   return (
     <div className='bg-[#FAFAFA] overflow-hidden'>
       <HeaderNav />
@@ -49,8 +37,8 @@ function App() {
       <JoinOurCommunity />
       <Comments />
       <Footer />
-      {/* <BackToTop/>
-      <LoaderHome/> */}
+      <BackToTop/>
+      <LoaderHome/>
     </div>
   )
 }
